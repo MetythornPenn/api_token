995e030282ef56a3537af98a9a8d5f9b52c60db2
xyber <metythorn@gmail.com>
2024-05-31 09:03:43 +0700

    :)

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-## Fastapi with API Token
+## Secure FastAPI using API Key with reverse proxy 
+
+
+### How to run service 
+
+```bash
+# build images
+docker compose up -d --build
+
+# test api with swagger 
+http://localhost:2000/docs
+
+```
+
+
+### Reference
+
+- [Medium article](https://nilsdebruin.medium.com/fastapi-authentication-revisited-enabling-api-key-authentication-122dc5975680)
+
+- [Sebastián's code](https://gist.githubusercontent.com/nilsdebruin/a78c5e200e7df014a92580b4fc51c53f/raw/7f18229ab35f843fe603b45ec9f7cc5f2b3c8d57/fastapi_api_key.py)
--- main.py
+++ main.py
@@ -1,47 +1,54 @@
-from fastapi import FastAPI, Depends, HTTPException, Security, Request
-from fastapi.security.api_key import APIKeyHeader
-from pydantic import BaseModel
-from starlette.status import HTTP_403_FORBIDDEN
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
+from fastapi import Security, Depends, FastAPI, HTTPException
+from fastapi.security.api_key import APIKeyQuery, APIKeyCookie, APIKeyHeader, APIKey
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.openapi.utils import get_openapi
 
-# Load environment variables from .env file
+from starlette.status import HTTP_403_FORBIDDEN
+from starlette.responses import RedirectResponse, JSONResponse
+
 load_dotenv()
 
 API_KEY: str = os.getenv("API_KEY")
 API_KEY_NAME: str = os.getenv("API_KEY_NAME")
+COOKIE_DOMAIN: str = os.getenv("COOKIE_DOMAIN")
 
-# If either of the variables is None, raise an error
-if not API_KEY or not API_KEY_NAME:
+if not API_KEY or not API_KEY_NAME or not COOKIE_DOMAIN:
     raise ValueError("API_KEY and API_KEY_NAME must be set in the environment variables")
 
-app = FastAPI()
-
-class SecureDataRequest(BaseModel):
-    data: str
-
+api_key_query = APIKeyQuery(name=API_KEY_NAME, auto_error=False)
 api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
+api_key_cookie = APIKeyCookie(name=API_KEY_NAME, auto_error=False)
 
-async def get_api_key(api_key_header: str = Security(api_key_header), request: Request = None):
-    print(f"Request headers: {request.headers}")
-    print(f"api key header: {api_key_header}")
-    print(f"api key .env: {API_KEY}")
-    print(f"api key name .env: {API_KEY_NAME}")
-    if api_key_header == API_KEY:
+
+async def get_api_key(
+    api_key_query: str = Security(api_key_query),
+    api_key_header: str = Security(api_key_header),
+    api_key_cookie: str = Security(api_key_cookie),
+):
+
+    if api_key_query == API_KEY:
+        return api_key_query
+    elif api_key_header == API_KEY:
         return api_key_header
+    elif api_key_cookie == API_KEY:
+        return api_key_cookie
     else:
         raise HTTPException(
             status_code=HTTP_403_FORBIDDEN, detail="Could not validate credentials"
         )
 
+
+# app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
+app = FastAPI()
+
+
 @app.get("/")
-async def read_root():
-    return {"message": "Welcome to the API"}
+async def homepage():
+    return "Welcome to the security test!"
 
-@app.get("/secure-data/")
-async def read_secure_data(api_key: str = Depends(get_api_key), query_param: str = None):
-    return {"message": "This is secured data", "query_param": query_param}
-
-@app.post("/secure-data/")
-async def create_secure_data(request: SecureDataRequest, api_key: str = Depends(get_api_key)):
-    return {"message": "You have created secured data", "data": request.data}
+@app.get("/secure_endpoint", tags=["test"])
+async def get_open_api_endpoint(api_key: APIKey = Depends(get_api_key)):
+    response = "How cool is this?"
+    return response
